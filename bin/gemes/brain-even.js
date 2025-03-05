6431d0052c366107a7bf097210f8bb9943182f70
Nuray <nuray.alirzava@gmail.com>
2025-03-05 20:01:25 +0300

    upd

--- bin/gemes/brain-even.js
+++ bin/gemes/brain-even.js
@@ -1,13 +1,32 @@
-#!/usr/bin/env node
-import logicGame from '../../src/index.js';
-
-const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
-
-const generateNumber = () => Math.floor(Math.random() * 50);
+import readlineSync from 'readline-sync';
 
 const isEven = (number) => number % 2 === 0;
 
-const getCorrectAnswer = (number) => (isEven(number) ? 'yes' : 'no');
+const playEvenGame = () => {
+  console.log('Welcome to the Brain Games!');
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+  for (let i = 0; i < 3; i += 1) {
+    const number = Math.floor(Math.random() * 100);
+    const correctAnswer = isEven(number) ? 'yes' : 'no';
+    const userAnswer = readlineSync.question(`Question: ${number}\nYour answer: `).trim().toLowerCase();
+
+    if (isNaN(number) || (userAnswer !== 'yes' && userAnswer !== 'no')) {
+      console.log("Invalid input. Please answer 'yes' or 'no'.");
+      return;
+    }
 
 
-console.log(logicGame(rules, generateNumber, getCorrectAnswer));
+    if (userAnswer !== correctAnswer) {
+      console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}".`);
+      console.log("Let's try again!");
+      return;
+    }
+
+    console.log('Correct!');
+  }
+
+  console.log('Congratulations, you won!');
+};
+
+export default playEvenGame;
